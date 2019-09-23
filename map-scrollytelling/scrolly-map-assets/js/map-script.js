2dbebebbe2a4cd3f9c8b729984495faa1c0e4bd2
feeblefruits <j.coetzee0@gmail.com>
2019-09-23 12:58:16 +0200

    Updated to latest content

--- map-scrollytelling/scrolly-map-assets/js/map-script.js
+++ map-scrollytelling/scrolly-map-assets/js/map-script.js
@@ -1,17 +1,10 @@
 var chapters = {
-'lesotho-eastern-cape': {
+'introduction': {
 duration: 3000,
 center: [25.0288, -24.0973],
 zoom: 4,
 pitch: 0,
 bearing: 0,
-},
-'introduction': {
-duration: 3000,
-center: [28.6790, -30.1115],
-zoom: 7.2,
-bearing: 20,
-pitch: 150,
 },
 'zimbabwe': {
 duration: 3000,
@@ -56,14 +49,12 @@ pitch: 70,
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWdkZXYiLCJhIjoiY2p4dzBpbnY1MDBnNzNrbXhqODhuNXBuOSJ9.WR7-Mdn3rIfJeps_BNUEBg';
 
-var { MapboxLayer, HexagonLayer } = deck;
-
 var start = [28.6790, -30.1115]
 
 //Create the Mapbox map
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10?optimize=true',
+    style: 'mapbox://styles/mapbox/streets-v11?optimize=true',
     center: start,
     zoom: 7.2,
     pitch: 10,
@@ -72,49 +63,6 @@ var map = new mapboxgl.Map({
 });
 
 map.scrollZoom.disable();
-
-// Get Data for visual
-data = d3.csv('https://raw.githubusercontent.com/feeblefruits/dig/master/data/claimants_coordinates_v3.csv')
-
-//Create the deck.gl hexagon layer and style for the data
-var COLOR_RANGE = [
-[238,115,101],
-[233,74,55],
-[213,44,24]
-];
-
-var LIGHT_SETTINGS = {
-    lightsPosition: [-0.144528, 49.739968, 8000, -3.807751, 54.104682, 8000],
-    ambientRatio: 0.4,
-    diffuseRatio: 0.6,
-    specularRatio: 0.2,
-    lightsStrength: [0.8, 0.0, 0.8, 0.0],
-    numberOfLights: 2
-};
-
-var hexagonLayer;
-
-//Add the deck.gl Custom Layer to the map once the Mapbox map loads
-scroll-map.on('style.load', () => {
-
-    hexagonLayer = new MapboxLayer({
-        id: 'heatmap',
-        type: HexagonLayer,
-        colorRange: COLOR_RANGE,
-        data: data,
-        radius: 10000,
-        coverage: 1,
-        upperPercentile: 99.5,
-        elevationScale: 1000,
-        extruded: true,
-        getPosition: d => [Number(d.Longitude), Number(d.Latitude)],
-        opacity: 0.9,
-        lightSettings: LIGHT_SETTINGS,
-    });
-
-    // Add the deck.gl hex layer below labels in the Mapbox map
-    map.addLayer(hexagonLayer, 'waterway-label');
-});
 
 window.onscroll = function() {
 var chapterNames = Object.keys(chapters);
@@ -126,7 +74,7 @@ break;
 }
 }
 };
- 
+
 var activeChapterName = 'lesotho-eastern-cape';
 function setActiveChapter(chapterName) {
 if (chapterName === activeChapterName) return;
